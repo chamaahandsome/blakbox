f0863081f064b02c1677f33a81dd16c4eec35f27
chamaahandsome <timboywa@gmail.com>
2024-06-23 17:34:32 -0400

    Refactor UI components and update dependencies in [domain]/[path]/page.tsx and [domain]/page.tsx

--- src/app/[domain]/[path]/page.tsx
+++ src/app/[domain]/[path]/page.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import ShopEditor from '@/app/(main)/vendor/[vendorId]/shops/[shopId]/editor/[shopPageId]/_components/shop-editor'
+import { getDomainContent } from '@/lib/queries'
+import EditorProvider from '@/providers/editor/editor-provider'
+import { notFound } from 'next/navigation'
+import React from 'react'
 
-const page = () => {
+const Page = async ({
+  params,
+}: {
+  params: { domain: string; path: string }
+}) => {
+  const domainData = await getDomainContent(params.domain.slice(0, -1))
+  const pageData = domainData?.ShopPages.find(
+    (page) => page.pathName === params.path
+  )
+
+  if (!pageData || !domainData) return notFound()
+
   return (
-    <div>
-      Path
-    </div>
+    <EditorProvider
+      vendorId={domainData.vendorId}
+      pageDetails={pageData}
+      shopId={domainData.id}
+    >
+      <ShopEditor
+        shopPageId={pageData.id}
+        liveMode={true}
+      />
+    </EditorProvider>
   )
 }
 
-export default page;
+export default Page
--- src/app/[domain]/page.tsx
+++ src/app/[domain]/page.tsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import { db } from '@/lib/db'
+import { getDomainContent } from '@/lib/queries'
+import EditorProvider from '@/providers/editor/editor-provider'
+import { notFound } from 'next/navigation'
+import React from 'react'
+import ShopEditorNavigation from '../(main)/vendor/[vendorId]/shops/[shopId]/editor/[shopPageId]/_components/shop-editor-navigation'
+import ShopEditor from '../(main)/vendor/[vendorId]/shops/[shopId]/editor/[shopPageId]/_components/shop-editor'
 
-const page = () => {
+const Page = async ({ params }: { params: { domain: string } }) => {
+  const domainData = await getDomainContent(params.domain.slice(0, -1))
+  if (!domainData) return notFound()
+
+  const pageData = domainData.ShopPages.find((page) => !page.pathName)
+
+  if (!pageData) return notFound()
+
+  await db.shopPage.update({
+    where: {
+      id: pageData.id,
+    },
+    data: {
+      visits: {
+        increment: 1,
+      },
+    },
+  })
+
   return (
-    <div>
-      Page
-    </div>
+    <EditorProvider
+      vendorId={domainData.vendorId}
+      pageDetails={pageData}
+      shopId={domainData.id}
+    >
+      <ShopEditor
+        shopPageId={pageData.id}
+        liveMode={true}
+      />
+    </EditorProvider>
   )
 }
 
-export default page;
+export default Page
