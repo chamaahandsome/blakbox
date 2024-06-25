5de82ffa940e7d2bb60b5f6a9b9c3c0f60430b59
chamaahandsome <timboywa@gmail.com>
2024-06-25 12:01:56 -0400

    Refactor database URLs and update dependencies

--- src/app/(main)/vendor/[vendorId]/shops/[shopId]/editor/[shopPageId]/_components/shop-editor/shop-editor-components/container.tsx
+++ src/app/(main)/vendor/[vendorId]/shops/[shopId]/editor/[shopPageId]/_components/shop-editor/shop-editor-components/container.tsx
@@ -65,7 +65,7 @@ const Container = ({ element }: Props) => {
             containerId: id,
             elementDetails: {
               content: {
-                src: 'https://www.youtube.com/embed/A3l6YYkXzzg?si=zbcCeWcpq7Cwf8W1',
+                src: 'https://www.youtube.com/embed/6rkIIL6R9Ks',
               },
               id: v4(),
               name: 'Video',
